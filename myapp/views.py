c8741faa2d7d21cf2498a934f8fe61dcf9d0dae0
TomCalde <tomascalderon@ug.uchile.cl>
2022-10-07 09:28:56 -0300

    Add placeholder

--- myapp/views.py
+++ myapp/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+
+from myapp.forms import CreateUserForm
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
     context = {
         'form': form
     }
