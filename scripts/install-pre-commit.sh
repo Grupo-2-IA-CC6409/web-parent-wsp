02d02e025d401b123912dc07dff7aefb1fb0b667
Gabriel Rojas <gabo06123@gmail.com>
2022-10-10 02:54:19 -0300

    feat: added some CI

---
+++ scripts/install-pre-commit.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+pre-commit install --hook-type pre-commit --hook-type pre-push
---
+++ scripts/reset.sh
@@ -0,0 +1,6 @@
+rm db.sqlite3
+python3 manage.py migrate
+if [ $1 ] && [ $1 == '-p' ]; then
+	echo "Populating db..."
+	python3 manage.py runscript populate_db
+fi
