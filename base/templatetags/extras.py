51568767be6207db1bf6648d5b73174033f9a107
TomCalde <tomascalderon@ug.uchile.cl>
2022-10-16 19:41:05 -0300

    print data msg

--- base/templatetags/extras.py
+++ base/templatetags/extras.py
@@ -8,10 +8,13 @@ register = template.Library()
 
 @register.simple_tag(name='get_qr')
 def get_qr():
-    
-    qrCode = requests.get("http://localhost:3000/get-qr")
-    img = qrcode.make(qrCode.json()['qr'])
-    image_io = io.BytesIO()
-    img.save(image_io, 'PNG')
-    dataurl = 'data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii')
-    return dataurl
+    try:
+        qrCode = requests.get("http://localhost:3000/get-qr")
+        img = qrcode.make(qrCode.json()['qr'])
+        image_io = io.BytesIO()
+        img.save(image_io, 'PNG')
+        dataurl = 'data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii')
+        return dataurl
+    except:
+        return 
+
