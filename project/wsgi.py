9d00a4345adb19d5fdeced66c077bf1cd64be455
Gabriel Rojas <gabo06123@gmail.com>
2022-11-08 15:53:50 -0300

    feat: add python-dotenv to manage env vars

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+from dotenv import load_dotenv
+
+# import .env
+project_folder = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(project_folder, ".env"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
